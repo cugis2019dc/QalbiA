e77723b0747273b95635841687d9acf7933e130e
Qalbi Ashford <53440599+QalbiAshford@users.noreply.github.com>
2019-07-29 12:45:51 -0400

    Add files via upload

---
+++ Qalbi's Spyder codes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import plotly
+Hello, my name is Qalbi
+print("Hello, my name is Qalbi")
+5*2
+5/2
+5-2
+5^2
+8/9*3
