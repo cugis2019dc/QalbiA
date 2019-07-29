c31300b7e9c0c4ba3071786681e1dbf67ccef885
Qalbi Ashford <53440599+QalbiAshford@users.noreply.github.com>
2019-07-29 13:57:11 -0400

    Add files via upload

---
+++ Qalbi.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import plotly
+Hello, my name is Qalbi
+print("Hello, my name is Qalbi")
+5*2
+5/2
+5-2
+5**2
+8/9*3
+print("5*2")
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+    
+multiply(4,5)
+
+def sum(a,b,c):
+    sum =  a*b*c 
+
+    print(sum)
+    
+sum(4,5,6)
+
+def area (b,c):
+    area = 1/2*b*c
+    
+    print("The area of a triangle with base", b, "and height", c, "is ", area)
+    
+area(2,8)
+
+
+
+
+    
